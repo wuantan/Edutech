c89ca7ba32de3e8829d89c96adec7c2a5375bbb1
CETECOM <CETECOM@AVAAOV02LC0413.duoc.acad>
2025-04-23 17:11:29 -0400

    Edutech clase curso bg

--- src/main/java/com/example/Edutech/Curso.java
+++ src/main/java/com/example/Edutech/Curso.java
@@ -1,5 +1,55 @@
 package com.example.Edutech;
 
 public class Curso {
+    private string id_curso, nombre, descripccion, id_instructor, categoria;
 
+    public Curso(string id_curso, string nombre, string descripccion, string id_instructor, string categoria) {
+        this.id_curso = id_curso;
+        this.nombre = nombre;
+        this.descripccion = descripccion;
+        this.id_instructor = id_instructor;
+        this.categoria = categoria;
+    }
+
+    public string getId_curso() {
+        return id_curso;
+    }
+
+    public string getNombre() {
+        return nombre;
+    }
+
+    public string getDescripccion() {
+        return descripccion;
+    }
+
+    public string getId_instructor() {
+        return id_instructor;
+    }
+
+    public string getCategoria() {
+        return categoria;
+    }
+
+    public void setId_curso(string id_curso) {
+        this.id_curso = id_curso;
+    }
+
+    public void setNombre(string nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripccion(string descripccion) {
+        this.descripccion = descripccion;
+    }
+
+    public void setId_instructor(string id_instructor) {
+        this.id_instructor = id_instructor;
+    }
+
+    public void setCategoria(string categoria) {
+        this.categoria = categoria;
+    }:
+
+    
 }
